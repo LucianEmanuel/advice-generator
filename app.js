15bcf509f050c969b8fdb731a13e950b5f005dac
LucianEmanuel <103621779+LucianEmanuel@users.noreply.github.com>
2023-03-25 07:04:42 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const id = document.querySelector('span');
+const advice = document.querySelector('p');
+const btn = document.querySelector('.btn');
+const apiUrl = 'https://api.adviceslip.com/advice';
+
+async function getApi(url, para1, para2) {
+  const response = await fetch(url);
+  let data = await response.json();
+  console.log(data);
+  para1.textContent = `Advice #"${data.slip.id}"`;
+  para2.textContent = `"${data.slip.advice}"`;
+}
+btn.addEventListener('click', e => {
+  e.preventDefault();
+  getApi(apiUrl, id, advice);
+});
